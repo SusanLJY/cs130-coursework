8fbd1b12ac94b62789cb8d42f7d907db57e55150
SusanLJY <70014546+SusanLJY@users.noreply.github.com>
2022-04-27 20:47:46 -0500

    homework2_hover_effect

---
+++ Onclass codes/lecture10/01-style-property-demo/main.js
@@ -0,0 +1,30 @@
+
+const makeRed = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector("body").style.backgroundColor = "red";
+    document.querySelector("#btn1").style.backgroundColor = "red";
+};
+
+const makeBlue = () => {
+    // your code here...
+    console.log('Change background to blue');
+    document.querySelector("body").style.backgroundColor = "blue";
+    document.querySelector("#btn2").style.backgroundColor = "blue";
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector("body").style.backgroundColor = "pink";
+    document.querySelector("#btn3").style.backgroundColor = "pink";
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector("body").style.backgroundColor = "orange";
+    document.querySelector("#btn4").style.backgroundColor = "orange";
+    // Don't just use button, instead, use the id of each button to refer.
+};
+
